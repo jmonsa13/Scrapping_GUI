19ced933eed065bcddc83c9f6c3a08a8b5aac1ef
jmonsalvo <jmonsa13@gmail.com>
2022-07-18 11:22:01 -0500

    Initial commit for GUI using Streamlit for the scrapping code

---
+++ Main_GUI_Price.py
@@ -0,0 +1,233 @@
+# Python project Dashboard for pricing
+# Creado por: Juan Monsalvo
+# ----------------------------------------------------------------------------------------------------------------------
+# Libraries import
+# ----------------------------------------------------------------------------------------------------------------------
+import os
+import urllib.request
+
+import math
+import pandas as pd
+
+import streamlit as st
+from PIL import Image
+from st_aggrid import AgGrid
+
+from plot_function import plot_price_history, plot_price_index
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Configuration and Global Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Function Definition
+# ----------------------------------------------------------------------------------------------------------------------
+def load_data(filename="Decorceramica_twopieces.csv"):
+    """
+    Función que carga el archivo csv guardado al conectar con la base de datos y devuelve un dataframe
+    """
+    df = pd.read_csv(filename)
+
+    return df
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Streamlit Setting
+# ----------------------------------------------------------------------------------------------------------------------
+st.set_page_config(page_title="Price Monitoring",
+                   initial_sidebar_state="collapsed",
+                   page_icon="📈",
+                   layout="wide")
+
+tabs = ["Summary Report", "USA Market", "List Price"]
+page = st.sidebar.radio("Pages", tabs, index=1)
+# ----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+# Initial page
+st.title('💲 Price Monitoring - Mansfield 📈')
+
+# Loading the files
+# Folder path definition
+directory = '.\\XX_Data'
+files_list = []
+for path, _, files in os.walk(directory):
+    for name in files:
+        files_list.append(os.path.join(path, name))
+
+# Empty data frame
+df = pd.DataFrame()
+
+# Loading the DF of each month in a unique DF
+for file in files_list:
+    df = pd.concat([df, load_data(filename=file)])
+
+# Dropping duplicates in case the robot take two values by day
+df.drop_duplicates(inplace=True)
+
+# Creating a new product name combining the product name + sku
+df['Producto_sku'] = ['_'.join(i) for i in zip(df['Producto'], df['SKU'].map(str))]
+
+# String the SKU
+df['SKU_str'] = df['SKU'].map(str)
+
+# ----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+if page == "USA Market":
+    st.header('1) Price Over Time')
+    st.subheader('Filters to apply')
+
+    # Layout
+    col1, col2, col3 = st.columns([1, 2, 2])
+    with col1:
+        filt1 = st.radio("Select one option", ['Marketplace', 'Brand', 'SKU', 'Price Range'], 0)
+
+    # SKU filters
+    if filt1 == 'SKU':
+        with col2:
+            sku_filter = st.text_input('Which SKU wants to visualize?', '135010007')
+            # Convert to number if possible
+            if sku_filter.isdigit():
+                sku_filter = int(sku_filter)
+            df_filter = df[df["SKU"] == sku_filter]  # 4021.101N.020, N2420, 135010007
+
+        if len(df_filter) == 0:
+            st.error(f"SKU {sku_filter} not found in dataset")
+
+    # Marketplace, Brand selection and Price Range
+    else:
+        with col2:
+            # Filtering by marketplace
+            if filt1 == 'Marketplace':
+                market_brand_sel = st.selectbox("Which marketplace wants to visualize?", df["Market_Place"].unique(), 0)
+                df_filter = df[df["Market_Place"] == market_brand_sel]
+
+            # Filtering by brand
+            elif filt1 == 'Brand':
+                market_brand_sel = st.selectbox("Which brands wants to visualize?", df["Fabricante"].unique(), 0)
+                df_filter = df[df["Fabricante"] == market_brand_sel]
+
+            # Range price
+            elif filt1 == 'Price Range':
+                price_range = st.slider('Select a range of prices', df['Precio'].min(), 1000.0,
+                                        (100.0, 200.0), step=1.0)
+
+                # Filtering the price by the last data
+                df_filter_aux = df[(df["Precio"] >= price_range[0]) & (df["Precio"] <= price_range[1]) &
+                                   (df["Fecha"] == df.iloc[-1]['Fecha'])]
+
+                df_filter = df[df['Producto_sku'].isin(list(df_filter_aux['Producto_sku'].unique()))]
+
+        with col3:
+            # filtering by format
+            market_brand_sel = st.selectbox("Which format wants to visualize?", ['All'] +
+                                            list(df_filter["Tipo"].unique()))
+            if market_brand_sel == 'All':
+                pass
+            else:
+                df_filter = df_filter[df_filter["Tipo"] == market_brand_sel]
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # Plotting line plot
+    if len(df_filter) == 0:
+        pass
+    else:
+        fig = plot_price_history(df=df_filter, group="Producto_sku", title="Price over Time")
+        st.plotly_chart(fig, use_container_width=True)
+
+        with st.expander("Explore data"):
+            AgGrid(df_filter[['Fecha', 'Producto_sku', 'Precio', 'Market_Place']],
+                   editable=False, sortable=True, filter=True, resizable=True, defaultWidth=5,
+                   fit_columns_on_grid_load=False, theme="streamlit",  # "light", "dark", "blue", "material"
+                   key="Toilet", reload_data=True,  # gridOptions=gridoptions,
+                   enable_enterprise_modules=True)
+
+        # --------------------------------------------------------------------------------------------------------------
+        # Information from the product
+        st.subheader("Information about the Product")
+
+        # Filtering by reference
+        ref = st.selectbox("¿Which reference wants to see?", list(df_filter["Producto_sku"].unique()))
+        product_ref = df_filter[df_filter["Producto_sku"] == ref]
+
+        # Requesting the image | Download image from URL if possible
+        try:
+            if math.isnan(product_ref["Image_url"].iloc[-1]):
+                image = Image.open('01_Ref_images/Empty.png')
+        except:
+            url_image = product_ref["Image_url"].iloc[-1].replace(" ", "%20")  # Replacing whitespace
+
+            # Introduccing header to avoid erro 404
+            opener = urllib.request.build_opener()
+            opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+            urllib.request.install_opener(opener)
+            try:
+                urllib.request.urlretrieve(url_image, "01_Ref_images/image.png")
+                # Loading image
+                image = Image.open('01_Ref_images/image.png')
+            except urllib.error.URLError as e:
+                print(e.__dict__)
+                # Loading image
+                image = Image.open('01_Ref_images/Empty.png')
+
+        # Plot image
+        st.image(image, caption='{} ({}) ${:,} {}'.format(product_ref["Producto"].iloc[-1],
+                                                          product_ref["SKU"].iloc[-1],
+                                                          product_ref["Precio"].iloc[-1],
+                                                          product_ref["Moneda"].iloc[-1]).replace(',', '.'),
+                 width=300)
+
+        # General information
+        st.markdown("**The url of the product is:** {}".format(product_ref["URL"].iloc[-1]))
+
+    # ----------------------------------------------------------------------------------------------------------------------
+    st.header('2) Comparison Products Mansfield')
+
+    # Reading files with the directory for comparisons
+    comp_df = pd.read_excel('XX_Master_database/Productos Mansfield.xlsx')
+
+    # Mansfield df
+    Mansfield_df = df[df["Fabricante"] == 'Mansfield']
+
+    cc1, cc2 = st.columns((1, 3))
+    # filtering by format
+    format_mansfield_sel = cc1.selectbox("Which format wants to compare?", ['All'] +
+                                         list(Mansfield_df["Tipo"].unique()))
+    if format_mansfield_sel == 'All':
+        pass
+    else:
+        Mansfield_df = Mansfield_df[Mansfield_df["Tipo"] == format_mansfield_sel]
+
+    # Mansfield product to compare
+    mansfield_product_sel = cc1.selectbox("Which Mansfield product wants to compare?",
+                                          Mansfield_df["Producto"].unique(), 0)
+
+    mansfield_product = Mansfield_df[Mansfield_df['Producto'] == mansfield_product_sel]
+    sku_mansfield = mansfield_product.iloc[-1]['SKU']
+
+    # Requesting the image
+    urllib.request.urlretrieve(mansfield_product["Image_url"].iloc[-1], "image.png")
+    image = Image.open('image.png')
+    cc1.image(image, caption='{} ({}) ${:,} {}'.format(mansfield_product["Producto"].iloc[-1],
+                                                       mansfield_product["SKU"].iloc[-1],
+                                                       mansfield_product["Precio"].iloc[-1],
+                                                       mansfield_product["Moneda"].iloc[-1]).replace(',', '.'),
+              width=300)
+
+    # General information
+    cc1.markdown("**The url of the product is:** {}".format(mansfield_product["URL"].iloc[-1]))
+
+    # Organizing the SKU
+    comp_df['Homologo'] = comp_df['Homologo Mansfield'].map(str)
+    comp_df['Homologo'] = comp_df['Homologo'].apply(lambda x: x.strip())
+
+    # Products to visualize
+    sku_comp = comp_df[comp_df['Homologo'] == str(sku_mansfield)]['Sku']
+
+    # Filter df
+    df_comp = df[df['SKU_str'].isin(list(sku_comp))]
+
+    # Plot price index
+    fig = plot_price_index(df=df_comp, group="Producto_sku", mansfield_prod=mansfield_product_sel,
+                           title=f"Mansfield Price index for {mansfield_product_sel}", orient_h=True)
+    cc2.plotly_chart(fig, use_container_width=True)
---
+++ plot_function.py
@@ -0,0 +1,185 @@
+# Python project Dashboard for pricing
+# Creado por: Juan Monsalvo
+# ----------------------------------------------------------------------------------------------------------------------
+# Libraries import
+# ----------------------------------------------------------------------------------------------------------------------
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Configuration and Global Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Function Definition
+# ----------------------------------------------------------------------------------------------------------------------
+def plot_price_history(df, group, title, orient_h=False):
+    """
+    Función que crea el gráfico de historico de precio.
+    :param df: data frame con los precios y la historia.
+    :param group: Texto para agrupar o dibujar por referencia o familia.
+    :param title: Título de la gráfica.
+    :param orient_h: Default FALSE, para poner los legend de manera horizontal.
+    :return: fig: Objeto de plotly para graficar externamente.
+    """
+    # Plotting line plot
+    fig = px.line(data_frame=df, x="Fecha", y="Precio", color=group, line_group=group,
+                  title=title,
+                  width=1000, height=600,
+                  labels={"Producto": "Product"},
+                  template="seaborn")
+
+    fig.update_traces(mode='lines+markers')
+    fig.update_layout(modebar_add=["v1hovermode", "toggleSpikeLines"])
+
+    fig.update_layout(
+        xaxis=dict(
+            rangeslider_visible=False,
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,
+                         label="1m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=3,
+                         label="3m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=1,
+                         label="YTD",
+                         step="year",
+                         stepmode="todate"),
+                    dict(step="all")
+                ])
+            ),
+            type="date"
+        )
+    )
+
+    if orient_h is True:
+        fig.update_layout(legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1),
+                          legend_title="Products")
+    fig.update_xaxes(
+        tickformatstops=[
+            dict(dtickrange=["d1", "d30"], value="%b %d\n%Y"),
+            dict(dtickrange=["d30", "d60"], value="%b '%y M"),
+            dict(dtickrange=["d60", "d90"], value="%b '%y M"),
+            dict(dtickrange=["M3", None], value="%Y Y")
+        ]
+    )
+    fig.update_xaxes(showline=True, linewidth=0.5, linecolor='black')
+
+    fig.update_yaxes(tickprefix="$", tickformat=",.2f")
+    fig.update_yaxes(showline=True, linewidth=0.5, linecolor='black')
+
+    # Set x-axis and y-axis title
+    fig['layout']['xaxis']['title'] = 'Date'
+    fig['layout']['yaxis']['title'] = "Price in USD"
+
+    return fig
+
+
+def plot_price_index(df, group, mansfield_prod, title, orient_h=False):
+    """
+    Función que crea el gráfico de historico de precio.
+    :param df: data frame con los precios y la historia.
+    :param group: Texto para agrupar o dibujar por referencia o familia.
+    :param mansfield_prod:
+    :param title: Título de la gráfica.
+    :param orient_h: Default FALSE, para poner los legend de manera horizontal.
+    :return: fig: Objeto de plotly para graficar externamente.
+    """
+
+    fig = make_subplots(rows=1, cols=2, subplot_titles=("Price over Time", "Price Positioning"),
+                        column_widths=[0.7, 0.3],
+                        )
+
+    # Color Scheme definition
+    line_color = ['rgba(255,127,0, 1)', 'rgba(44, 160, 44, 1)', 'rgba(214, 39, 40, 1)',
+                  'rgba(148, 103, 189, 1)', 'rgba(140, 86, 75, 1)',
+                  'rgba(227, 119, 194, 1)', 'rgba(127, 127, 127, 1)',
+                  'rgba(188, 189, 34, 1)', 'rgba(23, 190, 207,1)', 'rgba(31, 119, 180, 1)']
+
+    # Plotting line plot
+    product_order = []
+    for i, product in enumerate(df[group].unique()):
+        df_aux = df[df[group] == product]
+        product_order.append(product)
+        fig.add_trace(go.Scatter(x=df_aux['Fecha'], y=df_aux['Precio'], name=product, legendgroup=product,
+                                 line_color=line_color[i], mode='lines+markers'),
+                      row=1, col=1)
+
+    # Calculating the price index
+    df_index = df[df['Fecha'] == df['Fecha'].iloc[-1]][['Fecha', group, 'Producto', 'Precio']]
+    mansfield_ref = df_index[df_index['Producto'] == mansfield_prod]['Precio'].values
+
+    df_index['Price_index'] = (((mansfield_ref - df_index['Precio']) / df_index['Precio']) * 100) + 100
+
+    # Plotting scatter plot
+    for i, product in enumerate(product_order):
+        df_aux = df_index[df_index[group] == product]
+        fig.add_trace(go.Scatter(x=[mansfield_prod], y=np.round(df_aux['Price_index'], 2),
+                                 name=product, legendgroup=product,
+                                 line_color=line_color[i], mode='markers',
+                                 showlegend=False),
+                      row=1, col=2)
+
+    # Title and template
+    fig.update_layout(modebar_add=["v1hovermode", "toggleSpikeLines"], title_text=title, template="seaborn")
+
+    # Range date button
+    fig.update_layout(
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,
+                         label="1m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=3,
+                         label="3m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=1,
+                         label="YTD",
+                         step="year",
+                         stepmode="todate"),
+                    dict(step="all")
+                ])
+            ),
+            type="date"
+        )
+    )
+
+    if orient_h is True:
+        fig.update_layout(legend=dict(orientation="h", yanchor="top", y=-0.12,  xanchor="left", x=0.01),
+                          legend_title="Products")
+
+    # Update xaxis, yaxis properties
+    fig.update_xaxes(showline=True, linewidth=0.5, linecolor='black')
+    fig.update_yaxes(showline=True, linewidth=0.5, linecolor='black')
+
+    # Configuration first plot
+    fig.update_xaxes(rangeslider_visible=False, row=1, col=1)  # title_text="Date",
+    fig.update_xaxes(
+        tickformatstops=[
+            dict(dtickrange=["d1", "d30"], value="%b %d\n%Y"),
+            dict(dtickrange=["d30", "d60"], value="%b '%y M"),
+            dict(dtickrange=["d60", "d90"], value="%b '%y M"),
+            dict(dtickrange=["M3", None], value="%Y Y")
+        ], row=1, col=1
+    )
+    fig.update_yaxes(title_text="Price in USD", tickprefix="$", tickformat=",.2f", row=1, col=1)
+
+    # Configuration second plot
+    fig.update_xaxes(row=1, col=2)  # title_text="Mansfield Product",
+    fig.update_yaxes(title_text="% Price Index", ticksuffix="%", range=[50, 150], row=1, col=2)
+
+    # Hover
+    fig.update_traces(hovertemplate='%{y}',
+                      row=1, col=2)
+
+    return fig
